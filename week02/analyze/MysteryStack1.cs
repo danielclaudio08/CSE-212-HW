47d07d04eeb24fe6315ed1b290ee6af779d68bbe
danielclaudio08 <u262229862@byui.edu>
2025-09-18 12:41:38 +0800

    Analyze and Tests Activities

--- week02/analyze/MysteryStack1.cs
+++ week02/analyze/MysteryStack1.cs
@@ -1,13 +1,15 @@
-﻿public static class MysteryStack1 {
-    public static string Run(string text) {
-        var stack = new Stack<char>();
-        foreach (var letter in text)
-            stack.Push(letter);
+﻿public static class MysteryStack1
+{
+  public static string Run(string text)
+  {
+    var stack = new Stack<char>();
+    foreach (var letter in text)
+      stack.Push(letter);
 
-        var result = "";
-        while (stack.Count > 0)
-            result += stack.Pop();
+    var result = "";
+    while (stack.Count > 0)
+      result += stack.Pop();
 
-        return result;
-    }
+    return result;
+  }
 }
